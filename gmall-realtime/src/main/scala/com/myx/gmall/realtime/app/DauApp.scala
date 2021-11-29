5ab6bfeddc53efdff67ed1afbcbc2cf6e89e8d97
mayx <mayx0210@163.com>
2021-11-29 17:50:36 +0800

    利用redis对采集的日志数据进行去重

--- gmall-realtime/src/main/scala/com/myx/gmall/realtime/app/DauApp.scala
+++ gmall-realtime/src/main/scala/com/myx/gmall/realtime/app/DauApp.scala
@@ -5,11 +5,12 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 import com.alibaba.fastjson.{JSON, JSONObject}
-import com.myx.gmall.realtime.utils.MyKafkaUtil
+import com.myx.gmall.realtime.utils.{MyKafkaUtil, MyRedisUtil}
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import redis.clients.jedis.Jedis
 
 /**
  * @Description: 日活业务
@@ -45,7 +46,38 @@ object DauApp {
       }
     }
     // 测试输出
-    jsonObjDStream.print()
+    // jsonObjDStream.print()
+
+    //通过Redis   对采集到的启动日志进行去重操作  方案1  采集周期中的每条数据都要获取一次Redis的连接，连接过于频繁
+    //redis类型:set  key：dau：2021-11-29  value: mid   expire   3600*24
+    jsonObjDStream.filter{
+      jsonObj => {
+        // 获取登录日期
+        val dt: String = jsonObj.getString("dt")
+        // 获取设备id
+        val mid: String = jsonObj.getJSONObject("common").getString("mid")
+        // 拼接Redis中保存的登录信息的key
+        val dauKey = "dau:" + dt
+        // 获取Jedis客户端
+        val jedis: Jedis = MyRedisUtil.getJedisClient()
+        // 从redis中判断当前的设置是否登录过
+        val isFirst: lang.Long = jedis.sadd(dauKey, mid)
+        // 设置redis的实效时间
+        if(jedis.ttl(dauKey) < 0) {
+          jedis.expire(dauKey,3600*24)
+        }
+        // 关闭连接
+        jedis.close()
+        if (isFirst == 1L) {
+          // 说明是第一次登录
+          true
+        } else {
+          // 说明已经登录过了
+          false
+        }
+      }
+    }
+
     ssc.start()
     ssc.awaitTermination()
   }
