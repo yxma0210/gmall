80cc8b6e8038332a012662f4a68b5ff6b5c13350
mayx <mayx0210@163.com>
2021-12-03 15:27:14 +0800

    搭建项目架构

---
+++ gmall-publisher/src/test/java/com/myx/gamll/publisher/GmallPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myx.gamll.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
