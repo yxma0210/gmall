a65ae515ee9d7342fa328e8ec9a067ae47b33577
mayx <mayx0210@163.com>
2021-12-15 11:54:55 +0800

    省份样例类

---
+++ gmall-realtime/src/main/scala/com/myx/gmall/realtime/bean/ProvinceInfo.scala
@@ -0,0 +1,13 @@
+package com.myx.gmall.realtime.bean
+
+/**
+ * @Description: 省份样例类
+ * @author: mayx
+ * @date: 2021/12/15 11:54
+ */
+case class ProvinceInfo (
+                          id:String,
+                          name:String,
+                          area_code:String,
+                          iso_code:String,
+                        )
